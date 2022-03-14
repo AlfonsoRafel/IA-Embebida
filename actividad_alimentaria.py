efaf5b482b9a5cb480158562c9c03aca7f588571
AlfonsoRafel <alfonso_rafel@hotmail.com>
2022-03-13 21:09:25 -0300

    proyecto

--- actividad_alimentaria.py
+++ actividad_alimentaria.py
@@ -5,6 +5,17 @@ import librosa
 import numpy as np
 from os.path import join
 import pandas as pd
+from sklearn.neural_network import MLPClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import balanced_accuracy_score
+import joblib
 
 def generate_dataset(datadir):
     file = 'actividad_alimentaria.wav'
@@ -70,13 +81,50 @@ def generate_dataset(datadir):
     df.to_csv(join('./predict/act_alimentaria', 'out_clas_alim.csv'), index=False) 
     print('File generated')
 
-def predict():
-    return NotImplemented
+
+def run(df):
+    model = joblib.load('./models/model_act.pkl')
+
+    for i in range(0, len(df.events)):
+        #data = data_raw.astype(np.float32)
+        X = np.array(df[['events' , 'perc_bite', 'perc_chew' , 'perc_chewbite']])
+        X = X.reshape(1, len(X))
+        Y = model.predict(X)
+        df['class'][i] = Y[0]
+    return df
         
 
 def train(datadir):
-    
-    return NotImplemented
+    df = pd.read_csv('./predict/act_alimentaria/out_clas_alim.csv', sep=',', header=0)
+    df['class'].replace(['Rumination', 'Grazing', 'End', 'noclass'], [0, 1, 2, 3], inplace= True)
+    df = df.drop(df[df['class'] == 3].index)
+    df = df.drop(df[df['class'] == 2].index)
+    X_train = df[['events' , 'perc_bite', 'perc_chew' , 'perc_chewbite']]
+    y_train = df['class']
+    # Genera una nueva particion desde el conjunto de train
+    X_train_opt, X_optim, y_train_opt, y_optim = train_test_split(X_train,
+                                                                y_train,
+                                                                test_size=0.33,
+                                                                random_state=42)
+
+        
+    # Se define el modelo
+    model = make_pipeline(
+                            StandardScaler(),
+                            #DecisionTreeClassifier(max_depth=5, min_samples_leaf=1),
+                            #MLPClassifier(random_state=42, max_iter=2000, early_stopping=False, n_iter_no_change=100)
+                            RandomForestClassifier(n_estimators=1, random_state=42)
+                        )
+
+    # Se entrena el modelo
+    model.fit(X_train_opt, y_train_opt)
+
+    # Se prueba con la partición de optimización
+    y_pred_optim = model.predict(X_optim)
+
+    sc = balanced_accuracy_score(y_optim, y_pred_optim)
+    print(f"{sc:.3f} balanced accuracy")
+    joblib.dump(model, './models/model_act.pkl')
 
 
 def main():
@@ -88,7 +136,7 @@ def main():
         help='Data root to be processed')
     args = arg_parser.parse_args()
     
-    generate_dataset(args.datadir)
+    train(args.datadir)
 
 
 if __name__ == '__main__':
--- pipeline.py
+++ pipeline.py
@@ -6,6 +6,13 @@ from detection import predict as det_pred
 from sklearn.metrics import balanced_accuracy_score
 import pandas as pd
 import numpy as np
+import librosa
+from os.path import join
+
+from classification import run as class_run
+from detection import run as det_run
+from actividad_alimentaria import run as act_run
+
 
 def pipeline(datadir):
     det_pred(datadir)
@@ -35,7 +42,64 @@ def validate(datadir):
     sc = balanced_accuracy_score(thruth.classes[0:len_min[0]], predictions.classes[0:len_min[0]])
     print(f"{sc:.3f} balanced accuracy")
 
+def pipeline_full(datadir, step):
     
+    total_events = []
+    perc_bite = []
+    perc_chew = []
+    perc_chewbite = []
+    part_classes = []
+    df = pd.DataFrame({'events' : [], 'perc_bite' : [], 'perc_chew' : [], 'perc_chewbite' : [], 'class' : []})
+
+    
+    offset = 0
+    duration_step = step * 60.0
+    duration = librosa.get_duration(filename=join(datadir))
+    steps = np.int(duration/duration_step)
+
+    for i in range(0, steps - 1):
+        print('Step: ', i)
+        offset = i * duration_step
+        data, sr = librosa.load(join(datadir), offset=offset, duration=duration_step)
+        Ts = 1/sr
+        N = data.shape[0]  
+        t = np.arange(offset,Ts*N + i * duration_step,Ts) 
+
+        event_list, data_filt = det_run(data, sr)
+        t_set = np.arange(0,Ts*N,Ts)
+        
+        output_list = class_run(data,event_list, sr, t_set)
+        output_array = np.array(output_list)
+        events = len(output_array)
+        total_events.append(len(output_array))
+        perc_bite.append(len(np.where(output_array == 0)[0])/events)
+        perc_chew.append(len(np.where(output_array  == 1)[0])/events)
+        perc_chewbite.append(len(np.where(output_array  == 2)[0])/events)
+    
+    df['events']= total_events
+    df['perc_bite']= perc_bite
+    df['perc_chew']= perc_chew
+    df['perc_chewbite']= perc_chewbite
+    
+    df_new = act_run(df)
+    start = []
+    end = []
+    label = []
+    for i in df_new.index:
+        start.append(i * 3 * 60.0)
+        end.append(i * 3 * 60.0 + 3*60.0)
+        if df_new['class'][i] == 0:
+            label.append('Rumination')
+        else:
+            label.append('Grazing')
+    df = pd.DataFrame({'start' : [], 'end' : [], 'class' : []})
+    df_new['start'] = start
+    df_new['end'] = end
+    df_new['class'] = label
+    df_new.sort_values(by=['start'], inplace=True)
+    df_new.to_csv('prediction.csv', index=False) 
+    print('Predicción Realizada')
+
 def main():
     arg_parser = argparse.ArgumentParser(description="")
     arg_parser.add_argument(
